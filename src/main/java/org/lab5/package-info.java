b34ddfd15af389b716da398badfd305ebf7b9c09
kioratulate <angelina-pevtsova@ya.ru>
2024-05-13 00:47:44 +0300

    Added package description for lab5 and edited README.md

---
+++ src/main/java/org/lab5/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes and interfaces related to dependency injection and
+ * inversion of control (IoC) implementations for Lab 5.
+
+ * Dependencies are specified in a properties file and are automatically instantiated
+ * based on annotations using reflection.
+ */
+package org.lab5;
